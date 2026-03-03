eb217bb42db6a29c8798a2baefdf376e83a31d3e
Eunice Atete <euniceatete0@gmail.com>
2026-03-03 22:56:01 +0200

    Assignment Operators Basics

---
+++ assignmentOp.py
@@ -0,0 +1,10 @@
+x = 10       # Assign 10 to x
+
+x += 5       # Same as x = x + 5  = 15
+x -= 3       # Same as x = x - 3  = 12
+x *= 2       # Same as x = x * 2  = 24
+x //= 4      # Same as x = x // 4 = 6
+x **= 2      # Same as x = x ** 2 = 36
+x %= 10      # Same as x = x % 10 = 6
+
+print(x)
