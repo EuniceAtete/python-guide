1648dc0659e800a9c1775cdc58e9cfeffc40068b
Eunice Atete <euniceatete0@gmail.com>
2026-03-03 22:01:28 +0200

    Data Types

---
+++ dataTypes.py
@@ -0,0 +1,20 @@
+# String - text in quotes
+name = "Eunice"
+
+# Integer - whole number
+score = 42
+
+# Float - decimal number
+height = 5.4
+
+# Complex - has a real and imaginary part (rarely used at first)
+c = complex(3)  # Output: (3+0j)
+
+# Boolean - True or False only
+is_student = True
+
+# Check the type of any variable like this
+print(type(name))      # <class 'str'>
+print(type(score))     # <class 'int'>
+print(type(height))    # <class 'float'>
+print(type(is_student))# <class 'bool'>
