d6fd0e99ce77b1eb815debf9074982e31d6f9a70
Eunice Atete <euniceatete0@gmail.com>
2026-03-03 22:54:32 +0200

    Comparison Operators Basics

---
+++ compOp.py
@@ -0,0 +1,11 @@
+x = 10
+y = 5
+
+print(x == y)   # Equal, False
+print(x != y)   # Not equal?, True
+print(x > y)    # Greater than?, True
+print(x < y)    # Less than?, False
+print(x >= 10)  # Greater or equal?, True
+print(x <= 4)   # Less or equal?, False
+
+# These return True or False — super useful in if statements
