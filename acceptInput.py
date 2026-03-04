73d5630823fa86b7a064cfb24f6217758d2cf208
Eunice Atete <euniceatete0@gmail.com>
2026-03-04 23:20:18 +0200

    Acceptingc:\Users\HP\OneDrive\Documents\Arcane\my-repos\python-guide\acceptInput.py

---
+++ acceptInput.py
@@ -0,0 +1,9 @@
+import time
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hi,{name}!")
+print(f"Guess how many years you'll be having next year")
+time.sleep(1)
+print(age+1)
