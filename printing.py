7026013277f16278a7d8ff622a7ff77915d97633
Eunice Atete <euniceatete0@gmail.com>
2026-03-06 21:31:30 +0200

    Strings

--- printing.py
+++ printing.py
@@ -9,3 +9,39 @@ print(f"My name is {name} and I am {age} years old.")
 # You can also format numbers
 price = 6.99
 print(f"The price is {price:.2f}")  # .2f = 2 decimal places
+
+
+#Printing with two statements on separate lines using one line of code
+
+print("My name is Eunice.");print("Nice to meet you!!")
+
+print("Hello","Git!") # There is a space in between by default
+
+#String concatenation
+print("Hello","from","Python","World!") #A space between each statement on one line
+print("Hello"+"from"+"Python"+ "World!") #No space between them
+
+#Strings, variables and numbers in one string
+
+name = "Jenny"
+age = 16
+print("Hi",name,"!")
+
+print("I am",name,"and I am",age,"years old!")
+
+#Length of the string
+
+print(len(name))
+
+#Inclusion and Exclusion of a string in a string
+
+print("nny" in name)
+print("me" in name)
+
+#Slicing Strings
+
+print(name[0:5]) #From index 0 to 5
+print(name[:4]) #From the start index to 4
+print(name[6:]) # From 6 to the end
+print(name[-7:-1]) # From the back, -1 is the last index
+print(name[::-1]) #Reverse string
