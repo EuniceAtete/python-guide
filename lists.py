13a72c5ee9487bbd1cc1b39a514258bdd56b0e2e
Eunice Atete <euniceatete0@gmail.com>
2026-03-19 23:53:58 +0200

    chore: adding and changing list items

--- lists.py
+++ lists.py
@@ -17,3 +17,16 @@ print(numbers[2])   # Third item
 
 # Negative indexing (from the end)
 print(numbers[-1])  # Last item
+
+# Changing List Items
+
+numbers[0] = 10
+print(numbers)
+
+#Adding items
+
+numbers.append(6)   # Add to end
+print(numbers)
+
+numbers.insert(1, 99)  # Insert at position
+print(numbers)
