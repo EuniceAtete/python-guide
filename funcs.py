b8fb15c45d0cee42b99426b8962bbff4c4461f8a
Eunice Atete <euniceatete0@gmail.com>
2026-03-19 23:49:04 +0200

    chore: add functions part with default, key arguments and exercise

--- funcs.py
+++ funcs.py
@@ -20,3 +20,28 @@ def add(a,b):
 
 print(add(9,15))
 
+def greet_with_default(name="Guest"):
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Alice")
+
+
+#Multiple Parameters
+
+
+def describe_person(name, age):
+    print(f"{name} is {age} years old.")
+
+describe_person("Eunice", 18)
+
+#Keyword Arguments
+
+describe_person(age=18, name="Eunice")
+
+
+
+def multiply(x, y):
+    return x * y
+
+print("Multiplication:", multiply(4, 5))
