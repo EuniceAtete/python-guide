18bf0265155c63291a13b53207bc3614739aa774
Eunice Atete <euniceatete0@gmail.com>
2026-03-19 23:47:08 +0200

    chore: add functions part

---
+++ funcs.py
@@ -0,0 +1,22 @@
+#USER-DEFINED FUNCTIONS
+#No parameters
+
+def greet():
+    print("Hello, welcome to Python!")
+
+# Calling the function
+greet()
+
+#Parameters
+
+def greet_user(name):
+    print(f"Hello, {name}!")
+
+greet_user("Eunice")
+
+#Return values
+def add(a,b):
+    return a + b
+
+print(add(9,15))
+
