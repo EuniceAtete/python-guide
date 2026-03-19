86f499e2337b9d8c6f2f47807a42a3c789bbee60
Eunice Atete <euniceatete0@gmail.com>
2026-03-19 23:52:24 +0200

    chore: add lists and accessing them(arrays)

---
+++ lists.py
@@ -0,0 +1,19 @@
+
+#Creating a List
+
+numbers = [1, 2, 3, 4, 5]
+names = ["Eunice", "Alice", "Bob"]
+
+print(numbers)
+print(names)
+
+
+
+# 2. Accessing List Items (Indexing)
+
+# Index starts at 0
+print(numbers[0])   # First item
+print(numbers[2])   # Third item
+
+# Negative indexing (from the end)
+print(numbers[-1])  # Last item
