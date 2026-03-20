a2730d63242a786fa7a3f09d75b32c5ac7505e8d
Eunice Atete <euniceatete0@gmail.com>
2026-03-20 22:39:13 +0200

    chore: reading the file content

--- files.py
+++ files.py
@@ -1,5 +1,14 @@
+# Creating a file
 file = open("file.txt","w")
 file.write("Hi. I am Eunice.")
 file.close()
 
 print("File written successfully")
+
+# Reading file content
+
+file = open("file.txt","r")
+fileContent = file.read()
+print("File Content:\n")
+print(fileContent)
+file.close()
