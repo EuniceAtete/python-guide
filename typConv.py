b67ca6ac5aed25f345a648aa34de1340d67972ac
Eunice Atete <euniceatete0@gmail.com>
2026-03-23 02:31:05 +0200

    chore: changing types in inputting to do mathematical ops

--- typConv.py
+++ typConv.py
@@ -7,3 +7,11 @@ print(z)
 
 a = str(100)
 print(a + " is now a string")
+
+
+length = int(input("Enter the length of your land: "))
+width = int(input("Enter the width of your land: "))
+
+print(f"The area of your land is {length*width}")
+
+#These ops can't be carried out unless it was converted
