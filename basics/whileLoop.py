7e34650a80c2c03c81e5b0391a5e46cd17ad8bcd
Eunice Atete <euniceatete0@gmail.com>
2026-03-25 22:23:37 +0200

    feat: add while loops to the basics

---
+++ basics/whileLoop.py
@@ -0,0 +1,15 @@
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1  # increase i by 1
+
+# Example 2: User input loop
+# Keeps asking until user types 'exit'
+
+command = ""
+
+while command != "exit":
+    command = input("Type 'exit' to stop: ")
+
+print("Loop ended")
