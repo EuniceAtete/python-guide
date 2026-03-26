dbb20a65da2ba013626b072faea1bf9cd3e2af04
Eunice Atete <euniceatete0@gmail.com>
2026-03-26 22:47:38 +0200

    docs: specific exceptions and errors

--- intermediate/exceptionHandling.py
+++ intermediate/exceptionHandling.py
@@ -6,3 +6,17 @@ try:
     print(10 / x)
 except:
     print("Something went wrong!")
+# Specific errors
+
+print("\nESpecific exceptions")
+
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+    print(result)
+
+except ValueError:
+    print("Invalid input! Please enter a number.")
+
+except ZeroDivisionError:
+    print("You cannot divide by zero.")
