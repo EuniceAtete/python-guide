f87533fe5cb10e04285d0872cabccc7fe5f8349f
Eunice Atete <euniceatete0@gmail.com>
2026-03-27 23:00:13 +0200

    docs: raising exceptions manually

---
+++ intermediate/decorators.py
@@ -0,0 +1,13 @@
+# Decorator example
+
+def uppercase_decorator(func):
+    def wrapper():
+        result = func()
+        return result.upper()
+    return wrapper
+
+@uppercase_decorator
+def greet():
+    return "hello world"
+
+print(greet())
