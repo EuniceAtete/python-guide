890cf53597f0d75be41fda2d8d29f0e7b9894601
Eunice Atete <euniceatete0@gmail.com>
2026-03-27 23:02:26 +0200

    feat: added decorators and decorator with arguments

--- intermediate/decorators.py
+++ intermediate/decorators.py
@@ -1,4 +1,4 @@
-# Decorator example
+# Basic decorator
 
 def uppercase_decorator(func):
     def wrapper():
@@ -11,3 +11,20 @@ def greet():
     return "hello world"
 
 print(greet())
+
+# Decorator with arguments
+
+def repeat_decorator(times):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@repeat_decorator(3)
+def say_hi(name):
+    print(f"Hi {name}!")
+
+print("\nDecorator with arguments:")
+say_hi("Eunice")
